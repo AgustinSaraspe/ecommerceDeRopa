538aa0cee982b97767eb337ade44bb32c0310051
Thiago <ts.facebook.ts@gmail.com>
2023-02-02 19:19:15 -0300

    menus separados en componentes

---
+++ front/src/components/menu/DeleteProducts.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import { Alert, Snackbar } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { postProduct } from "../../redux/actions/actions";
+
+export const DeleteProduct = () => {
+  const dispatch = useDispatch();
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+  };
+  const [input, setInput] = useState({
+    name: "",
+    price: "",
+    stock: "",
+    description: "",
+  });
+  const [search, setSearch] = useState("");
+
+  const clearInputs = (e) => {
+    //FUNCION PARA BORRAR CONTENIDO DE LOS INPUTS
+
+    setInput({
+      name: "",
+      price: "",
+      stock: "",
+      description: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    console.log(input);
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    console.log("search:", search);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (!search.length) {
+      return;
+    }
+    console.log(search);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    if (
+      input.name.length &&
+      input.price.length &&
+      input.stock.length &&
+      input.description.length
+    ) {
+      //AQUI PODRIAMOS MANEJAR UN POSIBLE ERROR EN EL BACKEND
+      console.log("successfull");
+      dispatch(postProduct(input));
+      clearInputs();
+      setTimeout(() => {
+        setMessage(messages.success);
+        setSeverity("success");
+        setOpen(true);
+      }, 500);
+    } else {
+      setTimeout(() => {
+        console.log("error");
+        setMessage(messages.errorInput);
+        setSeverity("error");
+        setOpen(true);
+      }, 500);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  //SNACKBARS
+  const [severity, setSeverity] = useState("success");
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const messages = {
+    success: "El producto se editó con exito!",
+    error: "El producto no se pudo editar!",
+    errorInput: "Por favor rellene todos los campos",
+  };
+
+  return (
+    <div>
+      <div className="productMenu" style={formStyle}>
+        <h2>Borrar Productos</h2>
+        <div className="deleteProducts">
+          <input
+            placeholder="Buscar producto"
+            name="search"
+            value={search}
+            onChange={handleSearch}
+          />
+          <button onClick={handleSearchSubmit} className="searchButton">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+      </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
---
+++ front/src/components/menu/Sales.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export const Sales = () => {
+  return (
+    <div>
+      <h1>SALES</h1>
+    </div>
+  );
+};
