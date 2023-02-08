fc409ff31fd02afe5e43782ff7b4d613403f8828
Thiago <ts.facebook.ts@gmail.com>
2023-02-08 16:43:32 -0300

    Terminado la vista de los productos con fotos

--- front/src/components/Products.js
+++ front/src/components/Products.js
@@ -6,9 +6,9 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import "../style/products.css";
 
 import { useLocalStorage } from "../useLocalStorage/useLocalStorage";
-
 
 //Redux
 import { useSelector, useDispatch } from "react-redux";
@@ -20,66 +20,60 @@ function Products() {
   const products = useSelector((state) => state.products);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-  
 
   const [cartLocalStore, setCartLocalStore] = useLocalStorage("cart", "");
-   
-  const hadleClick = (e, product) =>{
-    e.preventDefault();
-      
-    let existe = false;
-    let cantidadActual = 0; 
 
-    if(cart.length){
-  
-      for(let item of cart){
-            if(item.id === product.id){
-              existe = true;
-              cantidadActual = item.cantidad;
-            }
+  const handleClick = (e, product) => {
+    e.preventDefault();
+
+    let existe = false;
+    let cantidadActual = 0;
+
+    if (cart.length) {
+      for (let item of cart) {
+        if (item.id === product.id) {
+          existe = true;
+          cantidadActual = item.cantidad;
+        }
       }
 
-      if(existe){
-       console.log("cantidad", cantidadActual)
+      if (existe) {
+        console.log("cantidad", cantidadActual);
 
-       let newProduct = {
+        let newProduct = {
           ...product,
-          cantidad: cantidadActual + 1 
-        }
-         
-         cart.map((e)=>{
-          if(e.id === newProduct.id){
+          cantidad: cantidadActual + 1,
+        };
+
+        cart.map((e) => {
+          if (e.id === newProduct.id) {
             e.cantidad = cantidadActual + 1;
           }
-         })
-          
-         setCartLocalStore(cart);
-      }else{
-       let newProduct = {
+        });
+
+        setCartLocalStore(cart);
+      } else {
+        let newProduct = {
           ...product,
-          cantidad: 1
-        }
-        dispatch(addProductCart(addProductCart(newProduct)))
+          cantidad: 1,
+        };
+        dispatch(addProductCart(addProductCart(newProduct)));
       }
-
-    }else{
-
+    } else {
       let newProduct = {
         ...product,
-        cantidad: 1
-      }
-  
-      dispatch(addProductCart(addProductCart(newProduct)))
-    }
+        cantidad: 1,
+      };
 
-  }
-  
+      dispatch(addProductCart(addProductCart(newProduct)));
+    }
+  };
+
   //Uso dos useffect para que no se haga un infinite loop
   useEffect(() => {
     dispatch(getAllProducts());
   }, []);
 
-  
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -88,94 +82,119 @@ function Products() {
     }
   }, [products]);
 
-  console.log("producst",products);
+  console.log("producst", products);
 
-
-  useEffect(()=>{
-    if(cart){
-      console.log("cart", cart)
+  useEffect(() => {
+    if (cart) {
+      console.log("cart", cart);
       setCartLocalStore(cart);
     }
-  },[cart])
+  }, [cart]);
 
-
-return (
-  <div
-    style={
-      loading
-        ? {
-            margin: ".5rem",
-            backgroundColor: "#111111ee",
-          }
-        : {
-            backgroundColor: "#111111ee",
-            margin: ".5rem",
-            display: "grid",
-            gridTemplateColumns: "repeat(4,minmax(200px, 1fr))",
-          }
-    }
-  >
-    {!loading ? (
-      products.map((product) => {
-        return (
-          <Card
-            sx={{
-              maxWidth: 320,
-              margin: "1rem",
-              backgroundColor: "transparent",
-              border: "1px solid #ddd",
-            }}
-          >
-            <CardMedia sx={{ height: 140 }} image={""} title={product.name} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {product.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {product.description}
-              </Typography>
-            </CardContent>
-            <CardActions
+  return (
+    <div
+      style={
+        loading
+          ? {
+              margin: ".5rem",
+              backgroundColor: "#111111ee",
+            }
+          : {
+              backgroundColor: "#111111ee",
+              margin: ".5rem",
+              display: "grid",
+              gridTemplateColumns: "repeat(4,minmax(200px, 1fr))",
+            }
+      }
+    >
+      {!loading ? (
+        products.map((product) => {
+          return (
+            <Card
               sx={{
-                justifyContent: "center",
+                maxWidth: 320,
+                margin: "1rem",
+                backgroundColor: "#111",
+                border: "1px solid #ddd",
+                color: "#fff",
+                cursor: "pointer",
               }}
+              key={product.id}
             >
-              <Button
-                size="medium"
+              <CardMedia
+                sx={{ height: 220 }}
+                image={product.file}
+                title={product.name}
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    fontFamily: "verdana",
+                  }}
+                >
+                  {product.name}
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant="h6"
+                  component="div"
+                  sx={{
+                    color: "#ddd",
+                    fontWeight: "light",
+                  }}
+                >
+                  ${product.price}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    color: "#ddd",
+                    fontWeight: "light",
+                  }}
+                >
+                  {product.description}
+                </Typography>
+              </CardContent>
+              <CardActions
                 sx={{
-                  fontWeight: "bold",
-                  border: "2px solid",
+                  display: "flex",
                 }}
               >
-                Comprar
-              </Button>
-              <Button
-                size="small"
-                sx={{}}
-                onClick={(e) => {
-                  hadleClick(e,product)
-                }}
-              >
-                Agregar al carrito
-              </Button>
-            </CardActions>
-          </Card>
-        );
-      })
-    ) : (
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-        }}
-      >
-        <SkeletonItem />
-        <SkeletonItem />
-        <SkeletonItem />
-      </div>
-    )}
-  </div>
-);
+                <button
+                  className="buyProductBtn"
+                  // onClick={handleBuy}
+                >
+                  Comprar
+                </button>
+                <button
+                  className="cartProductBtn"
+                  onClick={(e) => {
+                    handleClick(e, product);
+                  }}
+                >
+                  <i class="fa-solid fa-cart-plus"></i>
+                </button>
+              </CardActions>
+            </Card>
+          );
+        })
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+          }}
+        >
+          <SkeletonItem />
+          <SkeletonItem />
+          <SkeletonItem />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Products;
